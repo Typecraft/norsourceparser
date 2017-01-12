48840190bf8328bc4106e4b703dc17c153f97f9b
Tormod Haugland <tormod.haugland@gmail.com>
2017-01-12 11:36:00 +0100

    Updated travis.yml for deployment

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -21,7 +21,7 @@ except:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'tOgg1/norsourceparser'
+GITHUB_REPO = 'Typecraft/norsourceparser'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
