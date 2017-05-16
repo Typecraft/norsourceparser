d8d0b7e7f5d653f7160352b1fe3656de5b5fd0c4
Tormod Haugland <tormod.haugland@gmail.com>
2017-05-16 15:27:06 +0200

    Bumpversion 1.0.0-rc0 -> 1.0.0-rc1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='norsourceparser',
-    version='1.0.0-rc0',
+    version='1.0.0-rc1',
     description="Simple conversion of Norsource-xml files into tc-xml.",
     long_description=readme + '\n\n' + history,
     author="Tormod Haugland",
